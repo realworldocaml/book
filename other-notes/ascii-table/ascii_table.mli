b15d17e7192e20260874690268aab89f6f1e6430
Yaron Minsky <yminsky@janestreet.com>
2011-11-08 18:46:08 -0500

    ascii_table code

---
+++ other-notes/ascii-table/ascii_table.mli
@@ -0,0 +1,17 @@
+open Core.Std
+
+(** [render_table header data] returns a string of characters formatted as a table,
+    where [header] is the titles of each column, and [data] is a list of rows.  The
+    [header] and each element of [data] must be of the same length.  Uses UNIX-style
+    newline separators, i.e., `\n`
+*)
+val render_table
+  :  string list         (* header *)
+  -> string list list    (* data *)
+  -> string
+
+type 'a column
+val column : string -> ('a -> string) -> 'a column
+
+val render_columns :
+  'a column list -> 'a list -> string
