814b693aefe4eea8cd60fd7720e43c406b3dfc63
Anil Madhavapeddy <anil@recoil.org>
2012-12-29 19:55:46 +0000

    server improvements: redirect to www.realworldocaml.org if thats not the input host (this is needed for the Javascript cross-site to work), and also add Github access_token authentication (not yet hooked in, but it does retrieve the username from Github).

--- scripts/config.ml
+++ scripts/config.ml
@@ -1,3 +1,9 @@
 let client_id = "1605562d79611cd6d8bc"
 let client_secret = "1cd154a261b6240827b89cd3a0461790ac27be85"
 let auth = Cohttp.Auth.Basic ("rwo", "Whirly2")
+
+(* List of allowed Github users per milestone *)
+let authors = ["yminsky";"jyh";"avsm";"andyoram"]
+let allowed_users = function
+  |"alpha1" -> authors
+  |_ -> authors
