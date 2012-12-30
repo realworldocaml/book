8836b2d84e64becc394f432e28ad34628a7537f6
Anil Madhavapeddy <anil@recoil.org>
2012-12-30 00:21:01 +0000

    rename add_parts to transform_pandocbook

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 mkdir -p _build
 opam config -env > _build/env.sh
 . ./_build/env.sh
-ocamlbuild -j 4 -use-ocamlfind add_parts.native html_code_highlight.native dump_paragraph_fragments.native
+ocamlbuild -j 4 -use-ocamlfind transform_pandocbook.native html_code_highlight.native dump_paragraph_fragments.native
