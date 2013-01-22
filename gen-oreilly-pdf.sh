98a3e5e19cdf084b31fc34aa62d2081c056b4d3a
Anil Madhavapeddy <anil@recoil.org>
2013-01-22 19:28:33 +0000

    add scripts to rebuild OReilly PDFs more easily

--- gen-oreilly-pdf.sh
+++ gen-oreilly-pdf.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
 set -xe
 
-REPO=git@github.com:yminsky/OCaml-Book
-LOCAL=~/src/git/avsm/OCaml-Book
-HTML=$LOCAL/build/en/html
-SVNR=~/src/svn/current
-cd $LOCAL
-git pull -u $REPO
-make clean
-make
-make oreilly
-REVID=`git rev-parse HEAD`
+SVNR=~/svn/current
+CURDIR=`pwd`
 cd $SVNR
 svn update
-cp $LOCAL/build/en/source/rwo-oreilly.xml $SVNR/book.xml
-svn commit -m "autocommit from $REPO $REVID orm:commitpdf"
+cp ${CURDIR}/build/en/source/rwo-oreilly.xml $SVNR/book.xml
+svn commit -m "autocommit from $REPO orm:commitpdf"
 sleep 60
 svn update
-cp pdf/book.xml.pdf $HTML/rwo-snapshot.pdf
-cp pdf/.buildlog $HTML/buildlog.txt
-
+cp pdf/book.xml.pdf ${CURDIR}/book.pdf
+cp pdf/.buildlog ${CURDIR}/buildlog.txt
+cat buildlog.txt
