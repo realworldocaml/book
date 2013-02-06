27d8b9a3113074314578898948dcec99049076b6
Anil Madhavapeddy <anil@recoil.org>
2013-02-06 00:41:47 +0000

    add cryptokit examples

---
+++ examples/objects/cryptokit/myhash.ml
@@ -0,0 +1,14 @@
+open Core.Std
+open Cryptokit
+
+let _ =
+  let hash_fn =
+    match Filename.basename Sys.argv.(0) with
+    |"md5" -> Hash.md5 ()
+    |"sha1" -> Hash.sha1 ()
+    |_ -> Hash.md5 ()
+  in
+  In_channel.(input_all stdin) |!
+  hash_string hash_fn |!
+  transform_string (Hexa.encode ()) |!
+  print_endline
