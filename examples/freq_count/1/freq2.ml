3f1216ade16b2c4007eafd943aac997c15caa744
Yaron Minsky <yminsky@gmail.com>
2013-03-16 08:07:30 -0400

    updated freq_count example

--- examples/freq_count/1/freq2.ml
+++ examples/freq_count/1/freq2.ml
@@ -1,27 +1,17 @@
 open Core.Std
 
-
-(* [build_counts] computes a frequency count of lines read from [stdin]  *)
 let build_counts () =
-  (* [f] takes in the current frequency count and the new line, and returns an
-     updated frequency count.  [In_channel.fold_lines] calls [f] once per line
-     in the file, returning the final frequency count produced. *)
   In_channel.fold_lines stdin ~init:[] ~f:(fun counts line ->
-    (* # of times this line has been seen --- 0 if the line is new *)
     let count =
       match List.Assoc.find counts line with
       | None -> 0
       | Some x -> x
     in
-    (* update the frequency count *)
     List.Assoc.add counts line (count + 1)
   )
 
 let () =
-  (* Compute the line counts *)
-  let counts = build_counts () in
-  (* Sort the line counts in descending order of frequency *)
-  let sorted_counts = List.sort ~cmp:(fun (_,x) (_,y) -> compare y x) counts  in
-  (* Print out the frequency in the sorted count list *)
-  List.iter (List.take sorted_counts 10) ~f:(fun (line,count) ->
-    printf "%3d: %s\n" count line)
+  build_counts ()
+  |> List.sort ~cmp:(fun (_,x) (_,y) -> compare y x)
+  |> (fun l -> List.take l 10)
+  |> List.iter ~f:(fun (line,count) -> printf "%3d: %s\n" count line)
--- examples/freq_count/4/freq2.ml
+++ examples/freq_count/4/freq2.ml
@@ -1,11 +1,12 @@
-(* freq.ml: using Counter *)
 open Core.Std
 
 let build_counts () =
-  In_channel.fold_lines stdin ~init:[] ~f:Counter.touch
+  In_channel.fold_lines stdin ~init:Counter.empty ~f:Counter.touch
 
 let () =
   build_counts ()
-  |> List.sort counts ~cmp:(fun (_,x) (_,y) -> Int.descending x y)
+  |> Counter.to_list
+  |> List.sort ~cmp:(fun (_,x) (_,y) -> Int.descending x y)
   |> (fun l -> List.take l 10)
   |> List.iter ~f:(fun (line,count) -> printf "%3d: %s\n" count line)
+
