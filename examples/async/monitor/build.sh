19266f20f1d12c0feaf3bea06760ece6251ab0b7
Anil Madhavapeddy <anil@recoil.org>
2013-03-30 16:37:03 +0000

    add nested monitor test

--- examples/async/monitor/build.sh
+++ examples/async/monitor/build.sh
@@ -2,5 +2,5 @@
 
 eval `opam config -env`
 
-ocamlbuild -use-ocamlfind monitor_handle_errors.byte monitor_try_with.byte
+ocamlbuild -use-ocamlfind monitor_handle_errors.byte monitor_try_with.byte monitor_try_with_nested.byte
 
