cab864229d5adb90e027f863facdc875814fc16f
Yaron Minsky <yminsky@gmail.com>
2013-04-07 11:35:23 -0400

    added build.sh

---
+++ examples/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+eval `opam config -env`
+
+echo $PATH
+echo $CAML_LD_LIBRARY_PATH
+echo `which ocamlbuild`
+
+for TARGET in $*
+do
+  ocamlbuild -use-ocamlfind $TARGET -cflags "-w @A-4-33-41-42-43"
+done
+
+
