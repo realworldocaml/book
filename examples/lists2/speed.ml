c55ab432e4feb227debf162ce638c3f90961976c
Yaron Minsky <yminsky@gmail.com>
2013-04-07 11:11:57 -0400

    updated examples

--- examples/lists2/speed.ml
+++ examples/lists2/speed.ml
@@ -2,7 +2,7 @@ open Core.Std
 module Bench = Core_extended.Std.Bench
 module Test = Bench.Test
 
-let f x =
+let num_match x =
   match x with
   | 0 -> 1
   | 1 -> 2
@@ -16,8 +16,8 @@ let f x =
   | 9 -> 10
   | _ -> 11
 
-let g x =
-  if x = 0 then 1
+let num_if x =
+  if      x = 0 then 1
   else if x = 1 then 2
   else if x = 2 then 3
   else if x = 3 then 4
@@ -29,10 +29,35 @@ let g x =
   else if x = 9 then 10
   else 11
 
+let pos_match l =
+  match l with
+  | _ :: _ :: _ :: _ :: _ :: _ :: _ :: x :: _ -> x
+  | _ :: _ :: _ :: _ :: _ :: _ :: x :: _ -> x
+  | _ :: _ :: _ :: _ :: _ :: x :: _ -> x
+  | _ :: _ :: _ :: _ :: x :: _ -> x
+  | _ :: _ :: _ :: x :: _ -> x
+  | _ :: _ :: x :: _ -> x
+  | _ :: x :: _ -> x
+  | x :: _ -> x
+  | [] -> 0
+
+let pos_if l =
+  if      List.length l > 7 then List.nth_exn l 7
+  else if List.length l > 6 then List.nth_exn l 6
+  else if List.length l > 5 then List.nth_exn l 5
+  else if List.length l > 4 then List.nth_exn l 4
+  else if List.length l > 3 then List.nth_exn l 3
+  else if List.length l > 2 then List.nth_exn l 2
+  else if List.length l > 1 then List.nth_exn l 1
+  else if List.length l > 6 then List.nth_exn l 0
+  else 0
+
 let () =
   Exn.handle_uncaught ~exit:true (fun () ->
     Bench.bench
-      [ Test.create ~name:"match" (fun () -> ignore (f 9))
-      ; Test.create ~name:"if"    (fun () -> ignore (g 9))
+      [ Test.create ~name:"num match" (fun () -> ignore (num_match 9))
+      ; Test.create ~name:"num if"    (fun () -> ignore (num_if 9))
+      ; Test.create ~name:"pos match" (fun () -> assert (1 = pos_match [0;0;0;0;0;0;1]))
+      ; Test.create ~name:"pos if"    (fun () -> assert (1 = pos_if [0;0;0;0;0;0;1]))
       ]
   )
