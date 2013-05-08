40b5d68ade37e673c8ea0f3d43860af03e07f90c
Yaron Minsky <yminsky@gmail.com>
2013-05-08 18:21:15 -0400

    more async examples

--- examples/async/search_client/build_all.sh
+++ examples/async/search_client/build_all.sh
@@ -6,3 +6,6 @@
    search_out_of_order.native \
    search_with_error_handling.native \
    search_with_configurable_server.native \
+   search_with_timeout.native \
+   search_with_timeout_choice.native \
+    
