fc7a9d2fa6a636a0734a688d27545f0a10f80c7d
Yaron Minsky <yminsky@gmail.com>
2013-05-18 22:45:11 -0400

    example updates

--- examples/async/simple/build_all.sh
+++ examples/async/simple/build_all.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
 ../../build.sh \
-   search.native \
-   search_out_of_order.native \
-   search_with_configurable_server.native \
-   search_with_error_handling.native \
-   search_with_timeout.native \
-   search_with_timeout_choice.native \
-   search_with_timeout_no_leak_simple.native \
-   search_with_timeout_no_leak.native \
-    
+    scanner.byte
--- examples/async/simple/scanner.ml
+++ examples/async/simple/scanner.ml
@@ -1,12 +1,19 @@
-module Scanner : sig
+open Core.Std
+open Async.Std
+
+module Expect : sig
   type t
   val empty : t
   (* return a deferred that becomes determined as soon as a line is
      read that satisfied the condition in question *)
   val add_condition : t -> (string -> bool) -> t * string Deferred.t
-  val run : t -> Reader.t -> unit Deferred.t
+  (* Run the scanner, returning a scanner corresponding to the set of
+     unsatisfied conditions *)
+  val feed : t -> string -> t
+  val run : t -> Reader.t -> t Deferred.t
+
 end = struct
-  type t = (string -> bool * string Ivar.t) list
+  type t = ((string -> bool) * string Ivar.t) list
 
   let empty = []
 
@@ -15,11 +22,62 @@ end = struct
     let t' = (cond,ivar) :: t in
     (t',Ivar.read ivar)
 
-  let run t r =
-    Pipe.fold (Reader.lines r) ~init:t ~f:(fun t line ->
-      List.filter t ~f:(fun (cond,ivar) ->
-        if cond line
-        then (Ivar.fill ivar line; false)
-        else true))
+  let feed t line =
+    List.filter t ~f:(fun (cond,ivar) ->
+      if cond line then (Ivar.fill ivar line; false)
+      else true)
+
+  let run t r = Pipe.fold (Reader.lines r) ~init:t ~f:feed
 end
-;;
+
+
+module Sequencer : sig
+  type t
+  val create : unit -> t
+  val run : t -> (unit -> 'a Deferred.t) -> 'a Deferred.t
+end = struct
+  type t = (unit -> unit Deferred.t) Queue.t
+
+  let create () = Queue.create ()
+
+  let rec process_queue t =
+    match Queue.peek t with
+    | None -> ()
+    | Some thunk ->
+      thunk ()
+      >>> fun () ->
+      ignore (Queue.dequeue t);
+      if not (Queue.is_empty t) then process_queue t
+
+  let run t thunk =
+    Deferred.create (fun ivar ->
+      let was_empty = Queue.is_empty t in
+      Queue.enqueue t (fun () ->
+        thunk () >>| fun x -> Ivar.fill ivar x);
+      if was_empty then process_queue t
+    )
+end
+
+module type Delayer_intf = sig
+  type t
+  val create : Time.Span.t -> t
+  val schedule : t -> (unit -> 'a Deferred.t) -> 'a Deferred.t
+end
+
+module Delayer : Delayer_intf = struct
+  type t = { delay: Time.Span.t;
+             jobs: (unit -> unit) Queue.t;
+           }
+
+  let create delay =
+    { delay; jobs = Queue.create () }
+
+  let schedule t thunk =
+    let ivar = Ivar.create () in
+    Queue.enqueue t.jobs (fun () ->
+      upon (thunk ()) (fun x -> Ivar.fill ivar x));
+    upon (after t.delay) (fun () ->
+      let job = Queue.dequeue_exn t.jobs in
+      job ());
+    Ivar.read ivar
+end
