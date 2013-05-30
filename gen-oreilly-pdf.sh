ca6d798f112b2754aeb62f5785144a5a3f5a9d71
Anil Madhavapeddy <anil@recoil.org>
2013-05-30 12:08:03 +0100

    more sleep time for sleepy oreilly

--- gen-oreilly-pdf.sh
+++ gen-oreilly-pdf.sh
@@ -7,7 +7,7 @@ cd $SVNR
 svn update
 cp ${CURDIR}/build/en/source/rwo-oreilly.xml $SVNR/book.xml
 svn commit -m "autocommit from $REPO orm:commitpdf"
-sleep 60
+sleep 90
 svn update
 cp pdf/book.xml.pdf ${CURDIR}/data/live_site/trunk/rwo-snapshot.pdf
 cp pdf/.buildlog ${CURDIR}/data/live_site/trunk/buildlog.txt
