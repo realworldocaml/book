7af490191be44833d08c12481c780f71de2f3227
Anil Madhavapeddy <anil@recoil.org>
2013-06-14 15:48:00 +0100

    copyright

--- scripts/rwoserver.ml
+++ scripts/rwoserver.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2012 Anil Madhavapeddy <anil@recoil.org>
+ * Copyright (c) 2012-2013 Anil Madhavapeddy <anil@recoil.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
