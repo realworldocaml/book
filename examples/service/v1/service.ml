26b75897da95adb6ff7f226aac6567cc81dd9993
Yaron Minsky <yminsky@gmail.com>
2013-06-14 08:05:09 -0400

    updated service example

--- examples/service/v1/service.ml
+++ examples/service/v1/service.ml
@@ -7,33 +7,41 @@ module type S = sig
   val handle_request : t -> Sexp.t -> Sexp.t Or_error.t
 end
 
+module type Instance = sig
+  module Service : S
+  val this : Service.t
+end
+
 module Bundle = struct
-  type t = { handlers: (Sexp.t -> Sexp.t Or_error.t) String.Table.t; }
+  type t = { instances: (module Instance) String.Table.t; }
 
   let create services =
-    let handlers = String.Table.create () in
-    List.iter services ~f:(fun service_m ->
-      let module Service = (val service_m : S) in
-      if Hashtbl.mem handlers Service.name then
-        failwith ("Attempt to register duplicate handler for "^Service.name);
-      let service = Service.create () in
-      Hashtbl.replace handlers ~key:Service.name
-        ~data:(fun sexp -> Service.handle_request service sexp)
+    let instances = String.Table.create () in
+    List.iter services ~f:(fun service ->
+      let module Instance = struct
+        module Service = (val service : S)
+        let this = Service.create ()
+      end
+      in
+      let key = Instance.Service.name in
+      if Hashtbl.mem instances key then
+        failwith ("Attempt to register duplicate handler for " ^ key);
+      Hashtbl.replace instances ~key ~data:(module Instance : Instance)
     );
-    {handlers}
+    {instances}
 
   let handle_request t sexp =
     match sexp with
     | Sexp.List [Sexp.Atom name;query] ->
-      begin match Hashtbl.find t.handlers name with
+      begin match Hashtbl.find t.instances name with
       | None -> Or_error.error_string ("Unknown service: "^name)
-      | Some handler ->
-        try handler query
+      | Some (module Instance) ->
+        try Instance.Service.handle_request Instance.this query
         with exn -> Error (Error.of_exn exn)
       end
     | _ -> Or_error.error_string "Malformed query"
 
-  let service_names t = Hashtbl.keys t.handlers
+  let service_names t = Hashtbl.keys t.instances
 
 end
 
--- examples/service/v2/service.ml
+++ examples/service/v2/service.ml
@@ -9,37 +9,46 @@ module type S = sig
   val handle_request : t -> Sexp.t -> Sexp.t Or_error.t
 end
 
-type 'config t = (module S with type config = 'config)
+module type Instance = sig
+  module Service : S
+  val this : Service.t
+end
+
+type 'a t = (module S with type config = 'a)
 
 module Bundle = struct
   type 'a service = 'a t
-  type t = { handlers: (Sexp.t -> Sexp.t Or_error.t) String.Table.t; }
+  type t = { instances: (module Instance) String.Table.t; }
 
-  let create services =
-    { handlers = String.Table.create () }
+  let create () =
+    { instances = String.Table.create () }
 
   let register (type config) t service config  =
-    let module Service = (val service : S with type config = config) in
-    if Hashtbl.mem t.handlers Service.name then
+    let module Instance = struct
+      module Service = (val service : S with type config = config)
+      let this = Service.create config
+    end
+    in
+    let key = Instance.Service.name in
+    if Hashtbl.mem t.instances key then
       Or_error.error_string
-        ("Attempt to register duplicate handler for "^Service.name)
-    else
-      let service = Service.create config in
-      Hashtbl.replace t.handlers ~key:Service.name
-        ~data:(fun sexp -> Service.handle_request service sexp);
+        ("Attempt to register duplicate handler for " ^ key)
+    else (
+      Hashtbl.replace t.instances ~key ~data:(module Instance : Instance);
       Ok ()
+    )
   ;;
 
   let unregister t service_name =
-    Hashtbl.remove t.handlers service_name
+    Hashtbl.remove t.instances service_name
 
   let handle_request t sexp =
     match sexp with
     | Sexp.List [Sexp.Atom name;query] ->
-      begin match Hashtbl.find t.handlers name with
+      begin match Hashtbl.find t.instances name with
       | None -> Or_error.error_string ("Unknown service: "^name)
-      | Some handler ->
-        try handler query
+      | Some (module Instance) ->
+        try Instance.Service.handle_request Instance.this query
         with exn -> Error (Error.of_exn exn)
       end
     | _ -> Or_error.error_string "Malformed query"
