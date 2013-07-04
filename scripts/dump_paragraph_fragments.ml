bb63e2ef79dc6efb5ad00208e42febc05189324f
Anil Madhavapeddy <anil@recoil.org>
2013-07-04 14:59:36 +0100

    simplify dump para frag

--- scripts/dump_paragraph_fragments.ml
+++ scripts/dump_paragraph_fragments.ml
@@ -1,7 +1,6 @@
 (* Dump the paragraph fragments and into a sexp *)
 (* Usage: cmd output-file [input file1] [input file2...] *)
 open Core.Std
-open Xml_tree
 open Para_frag
 
 let odir, files =
@@ -11,26 +10,16 @@ let odir, files =
 
 let ts = ref []
 
-let filter dtd file i =
-  let rec aux = function
-    | Element ((("","p"),attr),c) -> begin
-      match List.Assoc.find attr ("","id") with
-      | None -> ()
-      | Some pid -> begin
-        let buf = Buffer.create 1 in
-        let xo = Xmlm.make_output ~decl:false (`Buffer buf) in
-        let tag = mk_tag "blockquote" c in
-        out_tree xo (dtd,tag);
-        let t = { file=Filename.basename file; html=Buffer.contents buf} in
-        match List.Assoc.find !ts pid with
-        | Some _ -> failwith (pid ^ " clash!")
-        | None -> ts := List.Assoc.add !ts pid t
-      end
-    end
-    | Element (_p,c) -> List.iter ~f:aux c
-    | Data _ -> ()
-  in
-  aux i
+let dump_frags file attr c =
+  match List.Assoc.find attr ("","id") with
+  | None -> ()
+  | Some pid -> begin
+    let html = Cow.Html.to_string <:html<<blockquote>$c$</blockquote>&>> in
+    let t = { file=Filename.basename file; html } in
+    match List.Assoc.find !ts pid with
+    | Some _ -> failwith (pid ^ " clash!")
+    | None -> ts := List.Assoc.add !ts pid t
+   end
 
 let _ =
   Printf.printf "output file: %s\n" odir;
@@ -38,9 +27,9 @@ let _ =
   List.iter files ~f:(fun file ->
     let buf = In_channel.read_all file in
     try
-      let i = Xmlm.make_input ~entity:Xhtml.entity (`String (0,buf)) in
-      let (dtd,it) = in_tree i in
-      filter dtd file it
+      Xml_tree.read_document (open_in file) 
+      |> fun (_dtd,doc) ->
+      Xml_tree.iter ~tag:"p" ~f:(dump_frags file) doc
     with Xmlm.Error (_p,e) -> begin
       Printf.eprintf "FATAL: %s %s\n" file (Xmlm.error_message e);
       print_endline buf;
