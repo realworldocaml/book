90117ddc193ab21bb84e90c6b4c711cb72326f72
Anil Madhavapeddy <anil@recoil.org>
2013-07-04 15:00:11 +0100

    simplify html_code_highlight to use new xml_tree helper functions

--- scripts/html_code_highlight.ml
+++ scripts/html_code_highlight.ml
@@ -1,41 +1,19 @@
 open Core.Std
-open Xml_tree
 
-let subst_with_code_frag c =
-  let open Code_frag in
+let subst_with_code_frag _attr (c:Cow.Xml.t) : Cow.Xml.t =
   try
-    let t = of_string c in
-    let frag = In_channel.read_all (sprintf "code/_build/%s.%d.html" t.name t.part) in
-    let i = Xmlm.make_input ~entity:Xhtml.entity (`String (0,frag)) in
-    let (_dtd,it) = in_tree i in
-    Some it
-  with exn -> prerr_endline (Exn.to_string exn); None
-
-let rec filter i =
-  match i with
-  | Element ((("","pre"),attr),c) as e -> begin
-      match List.Assoc.find attr ("","id") with
-      | None -> e
-      | Some _pid -> begin
-          (* If there is a language attr, then it isn't a frag, so don't try to parse it *)
-          match List.Assoc.find attr ("","language") with
-          | Some _ -> e
-          | None -> begin
-            let buf = String.concat ~sep:"" (List.map ~f:Xml_tree.to_string c) in
-            match subst_with_code_frag buf with
-            | None -> e
-            | Some it -> it
-          end
-        end
-    end
-  | Element (p,c) -> Element (p, List.map ~f:filter c)
-  | Data _ as d -> d
+    let t = Code_frag.of_string (Cow.Html.to_string c) in
+    In_channel.read_all (sprintf "code/_build/%s.%d.html" t.Code_frag.name t.Code_frag.part)
+    |> Cow.Html.of_string
+  with exn ->
+    eprintf "WARNING bare <pre> found. This should be turned into a ```frag:\n %s\n%!" (Exn.to_string exn);
+    Code_frag.wrap_in_pretty_box ~part:0 "Unknown" "" c
 
 let () =
   try
-    let i = Xmlm.make_input ~entity:Xhtml.entity (`Channel stdin) in
-    let o = Xmlm.make_output ~decl:false (`Channel stdout) in
-    let (_dtd,it) = in_tree i in
-    let ot = filter it in
-    out_tree o (None,ot)
-  with Xmlm.Error (_p,e) -> print_endline (Xmlm.error_message e)
+    Xml_tree.read_document In_channel.stdin
+    |> fun (dtd, doc) -> Xml_tree.map ~tag:"pre" ~f:subst_with_code_frag doc
+    |> fun doc -> Xml_tree.write_document Out_channel.stdout dtd doc
+  with 
+  | Xmlm.Error (_p,e) ->
+    print_endline (Xmlm.error_message e)
