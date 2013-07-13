3c3f61cd969e5a8d209f9cf089429ea5861f6fdc
Anil Madhavapeddy <anil@recoil.org>
2013-07-13 20:11:24 +0100

    add scm scripts

---
+++ code/sexpr/build_test_interval.sh
@@ -0,0 +1,1 @@
+ocamlbuild -use-ocamlfind -tag thread -syntax camlp4o -pkg core -pkg core.syntax test_interval.native
---
+++ code/sexpr/comment_heavy.scm
@@ -0,0 +1,11 @@
+;; a line comment
+((this is included)
+ ; (this is commented out
+ (this stays)
+ #; (all of this is commented
+     out (even though it crosses lines.))
+  (and #| block delimiters #| which can be nested |#
+     will comment out
+    an arbitrary multi-line block))) |#
+   now we're done
+   ))
