af2e2dfd79d159388dbc90ae41cc5a47d3ac026a
Anil Madhavapeddy <anil@recoil.org>
2013-07-23 19:23:34 +0100

    port more classes; some errors in the examples

---
+++ code/classes/doc.ml
@@ -0,0 +1,86 @@
+type doc =
+    Heading of string
+  | Paragraph of text_item list
+  | Definition of string list_item list
+
+and text_item =
+    Raw of string
+  | Bold of text_item list
+  | Enumerate of int list_item list
+  | Quote of doc
+
+and 'a list_item = 
+  { tag: 'a;
+    text: text_item list }
+
+(* part 1 *)
+open Core.Std
+
+class ['a] folder = object(self)
+  method doc acc doc =
+    match doc with
+      Heading _ -> acc
+    | Paragraph text -> List.fold ~f:self#text_item ~init:acc text
+    | Definition list -> List.fold ~f:self#list_item ~init:acc list
+
+  method list_item: 'b. 'a -> 'b list_item -> 'a = 
+    fun acc {tag; text} ->
+      List.fold ~f:self#text_item ~init:acc text
+
+  method text_item acc bar =
+    match bar with
+      Raw _ -> acc
+    | Bold text -> List.fold ~f:self#text_item ~init:acc text
+    | Enumerate list -> List.fold ~f:self#list_item ~init:acc list
+    | Quote doc -> self#doc acc doc
+end
+
+(* part 2 *)
+class counter = object
+  inherit [int] folder as super
+
+  method list_item acc li = acc
+
+  method text_item acc ti =
+    let acc = super#text_item acc ti in
+    match ti with
+      Bold _ -> acc + 1
+    | _ -> acc
+end
+
+let count_doc = (new counter)#doc
+
+(* part 3 *)
+class ['a] folder = object(self)
+  method doc acc doc =
+    match doc with
+      Heading str -> self#heading acc str
+    | Paragraph text -> self#paragraph acc text
+    | Definition list -> self#definition acc list
+
+  method list_item: 'b. 'a -> 'b list_item -> 'a =
+    fun acc {tag; text} ->
+      List.fold ~f:self#text_item ~init:acc text
+
+  method text_item acc bar =
+    match bar with
+      Raw str -> self#raw acc str
+    | Bold text -> self#bold acc text
+    | Enumerate list -> self#enumerate acc list
+    | Quote doc -> self#quote acc doc
+
+  method private heading acc str = acc
+  method private paragraph acc text =
+    List.fold ~f:self#text_item ~init:acc text
+  method private definition acc list =
+    List.fold ~f:self#list_item ~init:acc list
+
+  method private raw acc str = acc
+  method private bold acc text = 
+    List.fold ~f:self#text_item ~init:acc text
+  method private enumerate acc list = 
+    List.fold ~f:self#list_item ~init:acc list
+  method private quote acc doc = self#doc acc doc
+end
+
+let f : < doc : 'a; .. >  = new folder
