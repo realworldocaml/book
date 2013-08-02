f28d2aa00535c81587be4d44828eae722cb88bda
Anil Madhavapeddy <anil@recoil.org>
2013-08-02 02:41:42 +0100

    Int -> `Int
    closes ocamllabs/rwo-comments#1239

--- code/parsing/parsed_example.ml
+++ code/parsing/parsed_example.ml
@@ -1,5 +1,5 @@
 `Assoc
   ["title", `String "Cities";
    "cities", `List
-     [`Assoc ["name", `String "Chicago"; "zips", `List [Int 60601]];
-      `Assoc ["name", `String "New York"; "zips", `List [Int 10004]]]]
+     [`Assoc ["name", `String "Chicago"; "zips", `List [`Int 60601]];
+      `Assoc ["name", `String "New York"; "zips", `List [`Int 10004]]]]
