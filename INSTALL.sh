504577975f2a8ef1fd86a64426e5bfe9066c249d
Anil Madhavapeddy <anil@recoil.org>
2013-08-06 00:59:46 +0100

    Add INSTALL script with right OPAM packages needed to build the examples

---
+++ INSTALL.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+opam install --yes \
+  core \
+  core_extended \
+  cryptokit \
+  core_bench \
+  atdgen \
+  async \
+  yojson \
+  textwrap \
+  cohttp \
+  async_graphics \
+  menhir 
