75e8f98961e37710e4335481ade3f9f498be86fc
Anil Madhavapeddy <anil@recoil.org>
2013-08-05 15:56:37 +0100

    candidate build_embed_native.sh fix for ocamllabs/rwo-comments#1873

--- code/back-end-embed/build_embed_native.sh
+++ code/back-end-embed/build_embed_native.sh
@@ -1,3 +1,3 @@
 ocamlopt -output-obj -o embed_native.o embed_me1.ml embed_me2.ml
-gcc -Wall -I `ocamlc -where` -L `ocamlc -where` -lasmrun -ltermcap -o final_out.native embed_native.o main.c
+gcc -Wall -I `ocamlc -where` -o final_out.native embed_native.o main.c -L `ocamlc -where` -lasmrun -ltermcap -lm -ldl
 ./final_out.native
