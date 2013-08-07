03fa8debefd96588de9dbe59792b5d6a9081adff
Anil Madhavapeddy <anil@recoil.org>
2013-08-07 01:21:06 +0100

    add support for Async rewrite rules, borrowed from uTop. thanks @diml!

--- bin/rwo_run_toplevel.ml
+++ bin/rwo_run_toplevel.ml
@@ -1,6 +1,7 @@
 (* Syntax hightlight code and eval ocaml toplevel phrases.
  * Based on code from http://github.com/ocaml/ocaml.org 
  * Modified by Anil Madhavapeddy in 2013 for Real World OCaml and to use Core
+ * Much code borrowed from uTop.
  * TODO: license?
  *)
 
@@ -54,6 +55,143 @@ let flush_std_out_err () =
   Format.pp_print_flush Format.err_formatter ();
   flush stderr
 
+let with_loc loc str = {
+  Location.txt = str;
+  Location.loc = loc;
+}
+
+(** Rewrite rules, lifted straight from uTop. *)
+
+(* A rule for rewriting a toplevel expression. *)
+type rewrite_rule = {
+  required_values : Longident.t list;
+  (* Values that must exist and be persistent for the rule to apply. *)
+  rewrite : Location.t -> Parsetree.expression -> Parsetree.expression;
+  (* The rewrite function. *)
+}
+
+(* Rewrite rules, indexed by the identifier of the type
+   constructor. *)
+let rewrite_rules : (Longident.t, rewrite_rule) Hashtbl.t = Hashtbl.create 42
+
+let longident_lwt_main_run = Longident.Ldot (Longident.Lident "Lwt_main", "run")
+let longident_async_core_thread_safe_block_on_async_exn =
+  Longident.parse "Async.Std.Thread_safe.block_on_async_exn"
+let longident_unit = Longident.Lident "()"
+
+(* Wrap <expr> into: fun () -> <expr> *)
+let wrap_unit loc e =
+  let i = with_loc loc longident_unit in
+  let p = {
+    Parsetree.ppat_desc = Parsetree.Ppat_construct (i, None, false);
+    Parsetree.ppat_loc = loc;
+  } in
+  {
+    Parsetree.pexp_desc = Parsetree.Pexp_function ("", None, [(p, e)]);
+    Parsetree.pexp_loc = loc;
+  }
+
+let () =
+  (* Rewrite Async.Std.Defered.t expressions to
+     Async_core.Thread_safe.block_on_async_exn (fun () -> <expr>). *)
+  Hashtbl.add rewrite_rules (Longident.parse "Async_core.Ivar.Deferred.t") {
+    required_values = [longident_async_core_thread_safe_block_on_async_exn];
+    rewrite = (fun loc e -> {
+      Parsetree.pexp_desc =
+        Parsetree.Pexp_apply
+          ({ Parsetree.pexp_desc = Parsetree.Pexp_ident
+              (with_loc loc longident_async_core_thread_safe_block_on_async_exn);
+             Parsetree.pexp_loc = loc },
+           [("", wrap_unit loc e)]);
+      Parsetree.pexp_loc = loc;
+    });
+  }
+
+(* Returns whether the argument is a toplevel expression. *)
+let is_eval = function
+  | { Parsetree.pstr_desc = Parsetree.Pstr_eval _ } -> true
+  | _ -> false
+
+(* Returns whether the given path is persistent. *)
+let rec is_persistent_path = function
+  | Path.Pident id -> Ident.persistent id
+  | Path.Pdot (p, _, _) -> is_persistent_path p
+  | Path.Papply (_, p) -> is_persistent_path p
+
+(* Convert a path to a long identifier. *)
+let rec longident_of_path path =
+  match path with
+    | Path.Pident id ->
+      Longident.Lident (Ident.name id)
+    | Path.Pdot (path, s, _) ->
+      Longident.Ldot (longident_of_path path, s)
+    | Path.Papply (p1, p2) ->
+      Longident.Lapply (longident_of_path p1, longident_of_path p2)
+
+(* Returns the rewrite rule associated to a type, if any. *)
+let rec rule_of_type typ =
+  match typ.Types.desc with
+    | Types.Tlink typ ->
+      rule_of_type typ
+    | Types.Tconstr (path, _, _) -> begin
+      match try Some (Env.find_type path !Toploop.toplevel_env) with Not_found -> None with
+      | Some {
+        Types.type_kind = Types.Type_abstract;
+        Types.type_private = Asttypes.Public;
+        Types.type_manifest = Some typ;
+      } ->
+        rule_of_type typ
+      | _ ->
+        try
+          Some (Hashtbl.find rewrite_rules (longident_of_path path))
+        with Not_found ->
+          None
+    end
+    | _ ->
+      None
+
+(* Check that the given long identifier is present in the environment
+   and is persistent. *)
+let is_persistent_in_env longident =
+  try
+    is_persistent_path (fst (Env.lookup_value longident !Toploop.toplevel_env))
+  with Not_found ->
+    false
+
+let str_items_of_typed_structure tstr = tstr.Typedtree.str_items
+let str_desc_of_typed_str_item tstr = tstr.Typedtree.str_desc
+
+let rewrite_str_item pstr_item tstr_item =
+  match pstr_item, str_desc_of_typed_str_item tstr_item with
+    | ({ Parsetree.pstr_desc = Parsetree.Pstr_eval e;
+         Parsetree.pstr_loc = loc },
+       Typedtree.Tstr_eval { Typedtree.exp_type = typ }) -> begin
+      match rule_of_type typ with
+        | Some rule ->
+          if List.for_all is_persistent_in_env rule.required_values then begin
+            { Parsetree.pstr_desc = Parsetree.Pstr_eval (rule.rewrite loc e);
+              Parsetree.pstr_loc = loc }
+          end else
+            pstr_item
+        | None ->
+          pstr_item
+    end
+    | _ ->
+      pstr_item
+
+let rewrite phrase =
+  match phrase with
+    | Parsetree.Ptop_def pstr ->
+      if List.exists is_eval pstr then
+        let tstr, _, _ = Typemod.type_structure !Toploop.toplevel_env pstr Location.none in
+        let tstr = str_items_of_typed_structure tstr in
+        Parsetree.Ptop_def (List.map2 rewrite_str_item pstr tstr)
+      else begin
+        phrase
+      end
+    | Parsetree.Ptop_dir _ ->
+      phrase
+
 let toploop_eval phrase =
   if String.trim phrase = ";;" then `Normal("", "", "")
   else (
@@ -79,6 +217,7 @@ let toploop_eval phrase =
       lexbuf.Lexing.lex_start_p <- dummypos;
       lexbuf.Lexing.lex_curr_p <- dummypos;
       let phrase = !Toploop.parse_toplevel_phrase lexbuf in
+      let phrase = rewrite phrase in
       ignore(Toploop.execute_phrase true Format.str_formatter phrase);
       let exec_output = Format.flush_str_formatter () in
       let out, err = get_stdout_stderr_and_restore () in
@@ -99,6 +238,7 @@ let toploop_eval phrase =
       if not backtrace_enabled then Printexc.record_backtrace false;
       `Error(Format.flush_str_formatter ())
   )
+
 
 (*** Suppress values beginning with _.  Lifted straight from uTop:
  * uTop_main.ml
