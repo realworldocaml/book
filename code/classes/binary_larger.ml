61f20ffd6c53b6dce6aafdc85d77dcb1836d7e99
Leo White <lpw25@cl.cam.ac.uk>
2013-08-13 11:37:51 +0100

    Tidy up the new examples

--- code/classes/binary_larger.ml
+++ code/classes/binary_larger.ml
@@ -1,6 +1,5 @@
-class square w =
-  object (self) 
-    method width = w
-    method area = Float.of_int (self#width * self#width)
-    method larger other = self#area > other#area
-  end;;
+class square w = object(self) 
+  method width = w
+  method area = Float.of_int (self#width * self#width)
+  method larger other = self#area > other#area
+end
