17a543a211147e07afe2722cc1f34e771b211d71
Yaron Minsky <yminsky@gmail.com>
2013-08-16 21:29:36 -0400

    more async code

--- code/async/run_search_with_timeout_no_leak.sh
+++
@@ -1,2 +0,0 @@
-corebuild -pkg cohttp.async,yojson,textwrap search_with_timeout_no_leak.native
-./search_with_timeout_no_leak.native "concurrent programming" ocaml -timeout 0.1s
---
+++ code/async/thread_exp_async_busy_loop_in_thread.ml
@@ -0,0 +1,19 @@
+open Core.Std
+open Async.Std
+open Thread_exp_common
+
+
+(* part 1 *)
+let busy_loop n =
+  let x = ref None in
+  for i = 1 to 100_000_000 do x := Some i done
+
+let () =
+  don't_wait_for
+    (log_delays (In_thread.run busy_loop))
+
+(* part 2 *)
+let () = finish ()
+
+
+
