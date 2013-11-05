c70a9be8bff74fd2c1c982b4e6e34456c5bb399c
Anil Madhavapeddy <anil@recoil.org>
2013-11-05 10:24:17 -0500

    port html code highlight to work with latest sources

--- bin/rwo_html_code_highlight.ml
+++ bin/rwo_html_code_highlight.ml
@@ -17,20 +17,27 @@
 
 open Core.Std
 
-let subst_with_code_frag _attr (c:Cow.Xml.t) : Cow.Xml.t =
-  try
-    let t = Code_frag.of_string (Cow.Html.to_string c) in
-    In_channel.read_all (sprintf "code/_build/%s.%d.html" t.Code_frag.name t.Code_frag.part)
-    |> Cow.Html.of_string
-  with exn ->
-    eprintf "WARNING bare <pre> found. This should be turned into a ```frag:\n %s\n%!" (Exn.to_string exn);
-    exit 1
+let get_code_frag fname part =
+  let fname = sprintf "%s/%s.%d.html" Sys.argv.(1) fname part in
+  prerr_endline fname;
+  Ezxmlm.from_channel (open_in fname)
+  |> fun (dtd,xml) ->
+  xml
 
 let () =
-  try
-    Xml_tree.read_document In_channel.stdin
-    |> fun (dtd, doc) -> Xml_tree.map ~tag:"pre" ~f:subst_with_code_frag doc
-    |> fun doc -> Xml_tree.write_document Out_channel.stdout dtd doc
-  with
-  | Xmlm.Error (_p,e) ->
-    print_endline (Xmlm.error_message e)
+  let open Ezxmlm in
+  let dtd,xml = from_channel stdin in
+  filter_map ~tag:"pre" ~f:(fun attrs nodes -> []) xml
+  |> filter_map ~tag:"p" ~f:(fun attrs nodes ->
+    (* Hunt for a <p> with a href to github *)
+    try
+       let attr,link = member_with_attr "a" nodes in
+       let part = try
+         Scanf.sscanf (String.lstrip (data_to_string nodes)) "(part %d)" (fun d -> d)
+        with _ -> 0 in
+       let fname = String.chop_prefix_exn ~prefix:"https://github.com/realworldocaml/examples/tree/v1/code/" (get_attr "href" attr) in
+       let orig_node = [make_tag "p" (attrs, nodes)] in
+       get_code_frag fname part
+    with _ -> nodes   
+  )
+  |> fun d -> print_endline (Cow.Html.to_string d)
