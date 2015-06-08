99513ca4ae9af2e39c7765c43130df9047085be4
Yaron Minsky <yminsky@gmail.com>
2015-06-08 18:29:20 -0400

    tweaks and whitespace

--- book/code/command-line-parsing/basic_md5.ml
+++ book/code/command-line-parsing/basic_md5.ml
@@ -7,15 +7,11 @@ let do_hash file =
     |> transform_string (Hexa.encode ())
     |> print_endline
   )
-
-
 (* part 1 *)
 let spec =
   let open Command.Spec in
   empty
   +> anon ("filename" %: string)
-
-
 (* part 2 *)
 let command =
   Command.basic
@@ -23,8 +19,7 @@ let command =
     ~readme:(fun () -> "More detailed information")
     spec
     (fun filename () -> do_hash filename)
-
-
 (* part 3 *)
 let () =
   Command.run ~version:"1.0" ~build_info:"RWO" command
+(* part 4 *)
