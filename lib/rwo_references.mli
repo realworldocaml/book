55d83ca99b1489775b20d3c985c124385376df4c
Anil Madhavapeddy <anil@recoil.org>
2017-09-02 14:22:29 +0100

    remove library code, now moved to github.com/realworldocaml/scripts

--- lib/rwo_references.mli
+++
@@ -1,3 +0,0 @@
-val is_reference: Rwo_html.item -> bool
-
-val add_reference: string -> Rwo_html.item -> Rwo_html.item 
--- lib/rwo_util.mli
+++
@@ -1,9 +0,0 @@
-(** Miscellaneous utilities. *)
-open! Core
-open Async
-
-(** Return all files in the given directory, including files in its
-    sub-directories. *)
-val find_files : string -> string list Deferred.t
-
-val string_pair_equal : (string * string) -> (string * string) -> bool
