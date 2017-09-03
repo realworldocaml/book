b6bc28df41fdb4d16b487f67c6a325725f0a7ee5
Anil Madhavapeddy <anil@recoil.org>
2017-09-03 15:06:19 +0100

    update inferred type for qsort ctypes

--- code/ffi/qsort/qsort.mli
+++ code/ffi/qsort/qsort.mli
@@ -1,7 +1,10 @@
-val compare_t : (unit Ctypes.ptr -> unit Ctypes.ptr -> int) Ctypes.fn
+val compare_t :
+  (unit Ctypes_static.ptr -> unit Ctypes_static.ptr -> int) Ctypes_static.fn
 val qsort :
-  unit Ctypes.ptr ->
+  unit Ctypes_static.ptr ->
   PosixTypes.size_t ->
-  PosixTypes.size_t -> (unit Ctypes.ptr -> unit Ctypes.ptr -> int) -> unit
-val qsort' : ('a -> 'a -> int) -> 'a Ctypes.carray -> 'a Ctypes.carray
+  PosixTypes.size_t ->
+  (unit Ctypes_static.ptr -> unit Ctypes_static.ptr -> int) -> unit
+val qsort' :
+  ('a -> 'a -> int) -> 'a Ctypes_static.carray -> 'a Ctypes_static.carray
 val sort_stdin : unit -> unit
