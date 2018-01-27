b88d9a843eae7e40c8362c3024a1a01b22915be4
Yaron Minsky <yminsky@janestreet.com>
2018-01-27 08:57:05 -0500

    remove excess references to Core/Core_kernel

--- examples/code/files-modules-and-programs/abstract_username.ml
+++ examples/code/files-modules-and-programs/abstract_username.ml
@@ -4,8 +4,10 @@ module Username : sig
   type t
   val of_string : string -> t
   val to_string : t -> string
+  val (=) : t -> t -> bool
 end = struct
   type t = string
   let of_string x = x
   let to_string x = x
+  let (=) = String.(=)
 end
