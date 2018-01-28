8f57ad565c2901ac511edbed91eda7bdf22904b9
Yaron Minsky <yminsky@janestreet.com>
2018-01-28 11:33:24 -0500

    yet more Core_kernel to Base

--- examples/code/imperative-programming/time_converter2.ml
+++ examples/code/imperative-programming/time_converter2.ml
@@ -2,9 +2,9 @@ open Core
 
 let () =
   printf "Pick a timezone: %!";
-  match In_channel.input_line stdin with
+  match In_channel.input_line In_channel.stdin with
   | None -> failwith "No timezone provided"
   | Some zone_string ->
-    let zone = Zone.find_exn zone_string in
+    let zone = Time.Zone.find_exn zone_string in
     let time_string = Time.to_string_abs (Time.now ()) ~zone in
-    printf "The time in %s is %s.\n%!" (Zone.to_string zone) time_string
+    printf "The time in %s is %s.\n%!" (Time.Zone.to_string zone) time_string
