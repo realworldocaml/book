0ede1d6d4b33d77db0d385d67ec9962704911f1c
Thomas Gazagnaire <thomas@gazagnaire.org>
2018-03-06 12:20:51 +0100

    Replace jbuild.deps by cram parts in cal_add_sub_days example

--- examples/code/command-line-parsing/cal_add_sub_days/cal_add_sub_days.sh
+++ examples/code/command-line-parsing/cal_add_sub_days/cal_add_sub_days.sh
@@ -1,2 +1,6 @@
+### build
+  $ jbuilder build cal_add_sub_days.exe
+  $ ./_build/default/cal_add_sub_days.exe -help
+### run
   $ ./_build/default/cal_add_sub_days.exe add 2012-12-25 40
   $ ./_build/default/cal_add_sub_days.exe diff 2012-12-25 2012-11-01
