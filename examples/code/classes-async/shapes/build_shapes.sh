bb2798d1ba5bb20043d789d5ca2b0d8b9263d139
Yaron Minsky <yminsky@janestreet.com>
2018-03-23 15:55:50 -0400

    _

--- examples/code/classes-async/shapes/build_shapes.sh
+++ examples/code/classes-async/shapes/build_shapes.sh
@@ -1,6 +1,5 @@
   $ jbuilder build shapes.exe
-  File "jbuild", line 5, characters 2-72:
+  File "jbuild", line 7, characters 15-29:
   Error: Library "async_graphics" not found.
-  
   Hint: try: jbuilder external-lib-deps --missing shapes.exe
 @@ exit 1
