ab9c78c376165b0ac5ea4de50a3472bfc17238f4
Yaron Minsky <yminsky@janestreet.com>
2018-03-23 08:39:39 -0400

    more updates

--- examples/code/command-line-parsing/basic_md5_with_optional_file/basic_and_default_md5.sh
+++ examples/code/command-line-parsing/basic_md5_with_optional_file/basic_and_default_md5.sh
@@ -1,12 +1,8 @@
 ### build
   $ jbuilder build basic_md5_with_optional_file.exe
-      ocamlopt basic_md5_with_optional_file.exe
-  ld: warning: directory not found for option '-L/opt/local/lib'
   $ jbuilder build basic_md5_with_default_file.exe
-      ocamlopt basic_md5_with_default_file.exe
-  ld: warning: directory not found for option '-L/opt/local/lib'
 ### run
   $ cat /etc/passwd | ./_build/default/basic_md5_with_optional_file.exe
-  b88d621596b7e61337e832f7841066a9
+  27bf1f2dbadd4cae84f1da4dfe8b5cb3
   $ cat /etc/passwd | ./_build/default/basic_md5_with_default_file.exe
-  b88d621596b7e61337e832f7841066a9
+  27bf1f2dbadd4cae84f1da4dfe8b5cb3
