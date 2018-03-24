f2eea4aa8bf5064a3a0252f22b62e7dda87d0f26
Yaron Minsky <yminsky@janestreet.com>
2018-03-24 10:13:18 -0400

    more updates

--- examples/code/command-line-parsing/basic_md5_sequence/md5.ml
+++ examples/code/command-line-parsing/basic_md5_sequence/md5.ml
@@ -10,15 +10,14 @@ let command =
   Command.basic
     ~summary:"Generate an MD5 hash of the input data"
     ~readme:(fun () -> "More detailed information")
-    Command.Spec.(empty +> anon (sequence ("filename" %: file)))
-    (fun files () ->
-       match files with
-       | [] -> do_hash "-" In_channel.stdin
-       | _  ->
-         List.iter files ~f:(fun file ->
-           In_channel.with_file ~f:(do_hash file) file
-         )
-    )
+    Command.Let_syntax.(
+      let%map_open files = anon (sequence ("filename" %: file)) in
+      fun () ->
+        match files with
+        | [] -> do_hash "-" In_channel.stdin
+        | _ ->
+          List.iter files ~f:(fun file ->
+              In_channel.with_file ~f:(do_hash file) file))
 
 let () =
   Command.run ~version:"1.0" ~build_info:"RWO" command
