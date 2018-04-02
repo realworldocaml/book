926a94a6256432a9087e1c1471c8b4ab7a30ba8b
Thomas Gazagnaire <thomas@gazagnaire.org>
2018-04-02 18:59:38 +0200

    Fix rendering of OCaml snippets

--- bin/lib/rwo_pygments.mli
+++ bin/lib/rwo_pygments.mli
@@ -1,7 +1,6 @@
 (** Pygments support. Call out to the [pygmentize] command line
     tool. *)
 open Core
-open Async
 
 (** Pygments languages, just the ones we need. *)
 type lang = [
@@ -29,7 +28,8 @@ val of_lang : Rwo_lang.t -> lang Or_error.t
     If given, [add_attrs] will be added to the attributes <pre>.
 *)
 val pygmentize
-  :  ?add_attrs:Rwo_html.attributes
+  :  ?interactive:bool
+  -> ?add_attrs:Rwo_html.attributes
   -> lang
   -> string
-  -> Rwo_html.item Deferred.t
+  -> Rwo_html.item
