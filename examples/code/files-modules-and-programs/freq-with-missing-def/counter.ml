20b2dc0bb098b2cdaf9fc57bb8288faeb1adf553
Christophe Riolo <christophe.riolo@qvik.fi>
2018-06-01 14:19:56 +0300

    Corrected freq-with-missing-defs

--- examples/code/files-modules-and-programs/freq-with-missing-def/counter.ml
+++ examples/code/files-modules-and-programs/freq-with-missing-def/counter.ml
@@ -1,1 +1,15 @@
-type t
+open Base
+
+type t = int Map.M(String).t
+
+let empty = Map.empty (module String)
+
+let to_list t = Map.to_alist t
+
+let touch t s =
+  let count =
+    match Map.find t s with
+    | None -> 0
+    | Some x -> x
+  in
+  Map.set t ~key:s ~data:(count + 1)
--- examples/code/files-modules-and-programs/freq-with-missing-def/counter.mli
+++ examples/code/files-modules-and-programs/freq-with-missing-def/counter.mli
@@ -1,4 +1,18 @@
+open Base
+
+(** A collection of string frequency counts *)
 type t
+
+(** The empty set of frequency counts  *)
+val empty : t
+
+(** Bump the frequency count for the given string. *)
+val touch : t -> string -> t
+
+(** Converts the set of frequency counts to an association list.  A string shows
+    up at most once, and the counts are >= 1. *)
+val to_list : t -> (string * int) list
+
 
 [@@@part "1"]
 val count : t -> string -> int
