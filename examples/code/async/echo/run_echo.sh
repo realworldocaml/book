9b6da22b1c4b135eb65421a293ea62406f47745c
Christophe Riolo <christophe.riolo@qvik.fi>
2018-06-01 23:00:50 +0300

    Pass option to netcat to avoid freeze

--- examples/code/async/echo/run_echo.sh
+++ examples/code/async/echo/run_echo.sh
@@ -1,8 +1,8 @@
   $ dune build echo.exe
   $ ./_build/default/echo.exe &
   $ sleep 1
-  $ echo "This is an echo server" | nc 127.0.0.1 8765
+  $ echo "This is an echo server" | nc -N 127.0.0.1 8765
   This is an echo server
-  $ echo "It repeats whatever I write" | nc 127.0.0.1 8765
+  $ echo "It repeats whatever I write" | nc -N 127.0.0.1 8765
   It repeats whatever I write
   $ killall -9 echo.exe
