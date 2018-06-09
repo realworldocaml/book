3fb0dfaaa8690bff87d10988033c9c94448a3ec2
Christophe Riolo <christophe.riolo@qvik.fi>
2018-06-09 22:31:43 +0300

    Corrected ncurses examples

--- examples/code/ffi/ncurses/ncurses.ml
+++ examples/code/ffi/ncurses/ncurses.ml
@@ -7,37 +7,39 @@ let window : window typ = ptr void
 [@@@part "1"];;
 open Foreign
 
+let libncurses = Dl.(dlopen ~filename:"libncursesw.so" ~flags:[RTLD_NOW])
+
 let initscr =
-  foreign "initscr" (void @-> returning window)
+  foreign "initscr" (void @-> returning window) ~from:libncurses
 
 
 [@@@part "2"];;
 let newwin =
-  foreign "newwin" 
+  foreign "newwin"  ~from:libncurses
     (int @-> int @-> int @-> int @-> returning window)
 
 let endwin =
-  foreign "endwin" (void @-> returning void)
+  foreign "endwin" (void @-> returning void) ~from:libncurses
 
 let refresh =
-  foreign "refresh" (void @-> returning void)
+  foreign "refresh" (void @-> returning void) ~from:libncurses
 
 let wrefresh =
-  foreign "wrefresh" (window @-> returning void)
+  foreign "wrefresh" (window @-> returning void) ~from:libncurses
 
 let addstr =
-  foreign "addstr" (string @-> returning void)
+  foreign "addstr" (string @-> returning void) ~from:libncurses
 
 let mvwaddch =
-  foreign "mvwaddch"
+  foreign "mvwaddch" ~from:libncurses
     (window @-> int @-> int @-> char @-> returning void)
 
 let mvwaddstr =
-  foreign "mvwaddstr"
+  foreign "mvwaddstr" ~from:libncurses
     (window @-> int @-> int @-> string @-> returning void)
 
 let box =
-  foreign "box" (window @-> char @-> char @-> returning void)
+  foreign "box" (window @-> char @-> char @-> returning void) ~from:libncurses
 
 let cbreak =
-  foreign "cbreak" (void @-> returning int)
+  foreign "cbreak" (void @-> returning int) ~from:libncurses
