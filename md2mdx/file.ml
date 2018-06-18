6e467303d79eefe0ea42b3a132d0a71776d53453
Thomas Gazagnaire <thomas@gazagnaire.org>
2018-06-18 17:07:36 +0200

    Add md2mdx a tool to convert current split-files into one md file per chapter

---
+++ md2mdx/file.ml
@@ -0,0 +1,11 @@
+include Astring
+
+let read_lines file =
+  let ic = open_in file in
+  let r = ref [] in
+  try while true do r := input_line ic :: !r done; assert false
+  with End_of_file ->
+    close_in ic;
+    List.rev !r
+
+let read file = String.concat ~sep:"\n" (read_lines file)
