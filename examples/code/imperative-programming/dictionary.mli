a551a16bdf3c77279f823158595f4b49d5e41a82
Colin Leway <colin4124@gmail.com>
2019-01-23 10:46:25 +0800

    Add equality function as argument rather than polymorphic compare operator

--- examples/code/imperative-programming/dictionary.mli
+++ examples/code/imperative-programming/dictionary.mli
@@ -9,6 +9,6 @@ type ('a, 'b) t
 val create : unit -> ('a, 'b) t
 val length : ('a, 'b) t -> int
 val add    : ('a, 'b) t -> key:'a -> data:'b -> unit
-val find   : ('a, 'b) t -> 'a -> 'b option
+val find   : ('a, 'b) t -> equal:('a -> 'a -> bool) -> 'a -> 'b option
 val iter   : ('a, 'b) t -> f:(key:'a -> data:'b -> unit) -> unit
 val remove : ('a, 'b) t -> 'a -> unit
