c35c9211abea8d9025133a37298981151fa65dc8
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-08-14 15:44:01 +0200

    Bring .ml files back in sync with foreign-function-interface chapter

--- examples/code/ffi/return_frag.ml
+++ examples/code/ffi/return_frag.ml
@@ -1,17 +1,17 @@
 (* correct types *)
-val time: ptr time_t -> returning time_t
-val difftime: time_t -> time_t -> returning double
+val time: ptr time_t @-> returning time_t
+val difftime: time_t @-> time_t @-> returning double
 
+[@@@part "1"] ;;
 
-[@@@part "1"];;
 (* incorrect types *)
-val time: ptr time_t -> time_t
-val difftime: time_t -> time_t -> double
+val time: ptr time_t @-> time_t
+val difftime: time_t @-> time_t @-> double
 
+[@@@part "2"] ;;
 
-[@@@part "2"];;
 val curried : int -> int -> int
 
+[@@@part "3"] ;;
 
-[@@@part "3"];;
 val curried : int -> (int -> int)
