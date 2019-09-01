5c0478fd916aa35e350f4bf6ed6451ed665f3bb1
Yaron M. Minsky <yminsky@gmail.com>
2019-09-01 10:38:07 -0400

    small whitespace fix

--- examples/code/command-line-parsing/md5_sequence/md5.ml
+++ examples/code/command-line-parsing/md5_sequence/md5.ml
@@ -12,7 +12,8 @@ let command =
     ~readme:(fun () -> "More detailed information")
     Command.Let_syntax.(
       let%map_open files =
-        anon (sequence ("filename" %: Filename.arg_type)) in
+        anon (sequence ("filename" %: Filename.arg_type))
+      in
       fun () ->
         match files with
         | [] -> do_hash "-" In_channel.stdin
