c6fa9888f9b0c1bd7b476900a1e7f76cff7946cf
Yaron M. Minsky <yminsky@gmail.com>
2020-01-04 15:11:49 -0500

    _

---
+++ book/testing/examples/soup_test/test.ml
@@ -0,0 +1,9 @@
+open! Base
+open! Stdio
+
+let get_href_hosts soup =
+  Soup.select "a[href]" soup
+  |> Soup.to_list
+  |> List.map ~f:(Soup.R.attribute "href")
+  |> List.filter_map ~f:(fun uri -> Uri.host (Uri.of_string uri))
+  |> Set.of_list (module String)
