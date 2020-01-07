f0823d83283c3a3ff152700d5758c0bb95fb6250
Yaron M. Minsky <yminsky@gmail.com>
2020-01-07 07:37:11 -0500

    _

--- book/testing/examples/soup_test/test.ml
+++ book/testing/examples/soup_test/test.ml
@@ -8,5 +8,17 @@ let get_href_hosts soup =
   |> List.filter_map ~f:(fun uri -> Uri.host (Uri.of_string uri))
   |> Set.of_list (module String)
 
-let%expect_test "" =
-  print_endline "Whoa"
+let%expect_test _ =
+  let example_html = {|
+    <html>
+      Some random <b>text</b> with a
+      <a href="http://ocaml.org/base">link</a>.
+      And here's another
+      <a href="http://github.com/ocaml/dune">link</a>.
+      And here is <a>link</a> with no href.
+    </html>|}
+  in
+  let soup = Soup.parse example_html in
+  let hrefs = get_href_hosts soup in
+  print_s [%sexp (hrefs : Set.M(String).t)];
+  [%expect {| (github.com ocaml.org) |}]
