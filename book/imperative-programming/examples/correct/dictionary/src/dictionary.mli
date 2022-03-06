fc583c9c6f80f7849a93442b4a4355ea4bd34b6e
Yaron M. Minsky <yminsky@gmail.com>
2022-03-06 15:51:16 -0500

    ocamlformat

--- book/imperative-programming/examples/correct/dictionary/src/dictionary.mli
+++ book/imperative-programming/examples/correct/dictionary/src/dictionary.mli
@@ -4,11 +4,15 @@ open Base
 
 type ('a, 'b) t
 
-val create : hash:('a -> int) -> equal:('a -> 'a -> bool) -> ('a, 'b) t
+val create
+  :  hash:('a -> int)
+  -> equal:('a -> 'a -> bool)
+  -> ('a, 'b) t
+
 val length : ('a, 'b) t -> int
-val add    : ('a, 'b) t -> key:'a -> data:'b -> unit
-val find   : ('a, 'b) t -> 'a -> 'b option
-val iter   : ('a, 'b) t -> f:(key:'a -> data:'b -> unit) -> unit
+val add : ('a, 'b) t -> key:'a -> data:'b -> unit
+val find : ('a, 'b) t -> 'a -> 'b option
+val iter : ('a, 'b) t -> f:(key:'a -> data:'b -> unit) -> unit
 val remove : ('a, 'b) t -> 'a -> unit
 (* $MDX part-end *)
 
