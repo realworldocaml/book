77b7b4e02f403fe95686d0f8f06d6d3218235db2
Yaron M. Minsky <yminsky@gmail.com>
2022-04-03 19:36:38 -0400

    ocamlformat applied

--- book/testing/examples/correct/multi_block_expect_test/test.ml
+++ book/testing/examples/correct/multi_block_expect_test/test.ml
@@ -3,6 +3,6 @@ open Stdio
 
 let%expect_test "multi-block" =
   print_endline "Hello";
-  [%expect{| Hello |}];
+  [%expect {| Hello |}];
   print_endline "World!";
-  [%expect{| World! |}]
+  [%expect {| World! |}]
--- book/testing/examples/erroneous/quickcheck_property_test/test.ml
+++ book/testing/examples/erroneous/quickcheck_property_test/test.ml
@@ -1,9 +1,10 @@
 open Core
 
 let%test_unit "negation flips the sign" =
-  Quickcheck.test ~sexp_of:[%sexp_of: int]
+  Quickcheck.test
+    ~sexp_of:[%sexp_of: int]
     (Int.gen_incl Int.min_value Int.max_value)
     ~f:(fun x ->
-        [%test_eq: Sign.t]
-          (Int.sign (Int.neg x))
-          (Sign.flip (Int.sign x)))
+      [%test_eq: Sign.t]
+        (Int.sign (Int.neg x))
+        (Sign.flip (Int.sign x)))
